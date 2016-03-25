35bed21157a803789e57c294e65408c6671582c7
JJ Asghar <jj@chef.io>
2016-03-25 14:12:06 -0500

    Updating to 0.12.29 of headless server

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # The release you want to install of minecraft
-default['factorio']['version'] = '0.12.26'
+default['factorio']['version'] = '0.12.29'
 
 # The map location
 default['factorio']['map'] = '/srv/save/map'
