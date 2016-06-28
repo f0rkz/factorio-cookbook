e8ee1b190356b9feadd3674aa13e49ea27ce450f
JJ Asghar <jj@chef.io>
2016-06-28 17:37:07 -0500

    updated to 0.12.35

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # The release you want to install of minecraft
-default['factorio']['version'] = '0.12.29'
+default['factorio']['version'] = '0.12.35'
 
 # The map location
 default['factorio']['map'] = '/srv/save/map'
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jj@chef.io'
 license 'Apache v2'
 description 'Installs/Configures factorio server'
 long_description 'Installs/Configures factorio server'
-version '1.2.0'
+version '1.3.0'
 
 supports 'ubuntu', '>= 14.04'
 
